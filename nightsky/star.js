2cbe65d745e89ee5b1faa731ad650d8647a07721
Nicolas Hoffmann - PC Acer <nicolas.hoffmann@stud.h-da.de>
2019-01-16 13:39:57 +0100

    190116
    Added "star" class

--- nightsky/star.js
+++ nightsky/star.js
@@ -1,17 +1,15 @@
 
 /* jslint esversion: 6 */
 
-var picture = loadImage('stars.png');
-var x = width;
-var speed = 1;
+export default class star {
+    constructor(x, y){
+        this.pos = createVector(x, y);
+        this.size = 3;
+        this.luminance = 255;
+    }
 
-function draw(){
-    clear();
-    background(255);
-    image(picture, x, 0, width, height);
-    image(picture, x-width, 0, width, height);
-    x = x - speed;
-    if(x <= 0){
-        x = width;
+    update(){
+        fill(this.luminance);
+        ellipse(this.pos.x, this.pos.y, this.size);
     }
 }
