aa767e9083438aae30c0397c3db8f1b4b1126b48
Nicolas Hoffmann - PC Acer <nicolas.hoffmann@stud.h-da.de>
2019-01-16 14:30:35 +0100

    190116
    Corrected some copying mistakes

--- nightsky/static/sky(static).js
+++ nightsky/static/sky(static).js
@@ -1,14 +1,14 @@
 
 /* jslint esversion: 6 */
 
-import star from "star.js";
+import Star from "../star.js";
 
 var stars = [];
 var starAmount = 100;
 
 
 for(var i = 0; i < starAmount; i++){
-    stars.push(new star(random(0, width), random(0, height)));
+    stars.push(new Star(random(0, width), random(0, height)));
 }
 
 for(let star in stars){
